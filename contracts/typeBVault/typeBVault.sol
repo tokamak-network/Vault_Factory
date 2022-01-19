8dd013feecc8ae08ae09a4f5853a57cb0890417b
HyukSang <zzooppii@gmail.com>
2022-01-19 19:58:09 +0900

    develop the Vault Factory

---
+++ contracts/typeBVault/typeBVault.sol
@@ -0,0 +1,27 @@
+//SPDX-License-Identifier: Unlicense
+pragma solidity ^0.8.4;
+
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+import "../common/AccessibleCommon.sol";
+
+//import "hardhat/console.sol";
+
+contract typeBVault is AccessibleCommon {
+    using SafeERC20 for IERC20;
+
+    IERC20 public token;
+
+    constructor(address _token, address _admin) {
+        require(_token != address(0), "Vault: zero address");
+        token = IERC20(_token);
+        _setRoleAdmin(ADMIN_ROLE, ADMIN_ROLE);
+        _setupRole(ADMIN_ROLE, _admin);
+    }
+
+    function claim(address to, uint256 amount) external onlyOwner {
+        require(token.balanceOf(address(this)) >= amount,"Vault: insufficent");
+        token.safeTransfer(to, amount);
+    }
+
+}
