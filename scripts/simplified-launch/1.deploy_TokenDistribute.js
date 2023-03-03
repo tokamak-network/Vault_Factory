450b7628499be95a5a0f264b7835b3abef6048cd
zena <zena@tokamak.network>
2023-03-03 12:55:31 +0900

    Create 1.deploy_TokenDistribute.js

---
+++ scripts/simplified-launch/1.deploy_TokenDistribute.js
@@ -0,0 +1,55 @@
+const { ethers, run } = require("hardhat");
+const save = require("../save_deployed");
+const loadDeployed = require("../load_deployed");
+
+async function main() {
+    const accounts = await ethers.getSigners();
+    const deployer = accounts[0];
+    console.log("deployer: ", deployer.address);
+
+    const { chainId } = await ethers.provider.getNetwork();
+    let networkName = "local";
+    if(chainId == 1) networkName = "mainnet";
+    if(chainId == 4) networkName = "rinkeby";
+    if(chainId == 5) networkName = "goerli";
+
+    let deployInfo = {
+        name: "",
+        address: ""
+    }
+
+    let factory = await ethers.getContractFactory("TokenDistribute")
+    tokenDistribute = await factory.deploy();
+    await tokenDistribute.deployed()
+
+    console.log("TokenDistribute: ", tokenDistribute.address);
+
+    deployInfo = {
+        name: "TokenDistribute",
+        address: tokenDistribute.address
+    }
+
+    save(networkName, deployInfo);
+
+    if(chainId == 1 || chainId == 4 || chainId == 5) {
+
+      await run("verify", {
+        address: tokenDistribute.address,
+        constructorArgsParams: [],
+      });
+
+    }
+
+    console.log("TokenDistribute verified");
+
+}
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+
+main()
+  .then(() => process.exit(0))
+  .catch(error => {
+    console.error(error);
+    process.exit(1);
+  });
